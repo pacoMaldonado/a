b4bf7acd3d8e986fc2f1ef05ff443b3ad87d25d7
Javi Maldonado <javimaldonadohdez@gmail.com>
2023-09-19 02:26:51 -0600

    Simplicar carga del logo en el navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  OnInit,
-  inject,
-} from '@angular/core'
+import { Component, ElementRef, HostListener, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 @Component({
@@ -14,39 +8,9 @@ import { CommonModule } from '@angular/common'
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   private elementRef = inject(ElementRef)
   isDisplacement = false
-  getScreenWidth = 0
-  getScreenHeight = 0
-  screen = true
-  screenLg = false
-  screenXL = false
-
-  imgSize = '4rem'
-  imgSizeLg = '5rem'
-  imgSizeXL = '6rem'
-
-  logoStyle = {
-    width: this.imgSize,
-    heigth: this.imgSize,
-  }
-
-  logoStyleLg = {
-    width: this.imgSizeLg,
-    heigth: this.imgSizeLg,
-  }
-
-  logoStyleXL = {
-    width: this.imgSizeXL,
-    heigth: this.imgSizeXL,
-  }
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.screenSize()
-  }
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
@@ -61,57 +25,8 @@ export class NavbarComponent implements OnInit {
 
     if (scrollPosition === componentPosition - 128) {
       this.isDisplacement = false
-      this.imgSize = '4rem'
-      this.imgSizeLg = '5rem'
-      this.imgSizeXL = '6rem'
     } else {
       this.isDisplacement = true
-      this.imgSize = '3rem'
-      this.imgSizeLg = '4rem'
-      this.imgSizeXL = '5rem'
-    }
-
-    this.logoStyle = {
-      width: this.imgSize,
-      heigth: this.imgSize,
-    }
-
-    this.logoStyleLg = {
-      width: this.imgSizeLg,
-      heigth: this.imgSizeLg,
-    }
-
-    this.logoStyleXL = {
-      width: this.imgSizeXL,
-      heigth: this.imgSizeXL,
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onWindowResize() {
-    this.screenSize()
-  }
-
-  screenSize() {
-    this.getScreenWidth = window.innerWidth
-    this.getScreenHeight = window.innerHeight
-
-    if (this.getScreenWidth <= 800) {
-      this.screen = true
-    } else {
-      this.screen = false
-    }
-
-    if (this.getScreenWidth >= 801 && this.getScreenWidth <= 1023) {
-      this.screenLg = true
-    } else {
-      this.screenLg = false
-    }
-
-    if (this.getScreenWidth >= 1024) {
-      this.screenXL = true
-    } else {
-      this.screenXL = false
     }
   }
 }
