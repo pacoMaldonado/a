c6441131eae496c289b40b9f6a473714891b0bb3
Javi Maldonado <javimaldonadohdez@gmail.com>
2023-09-19 02:18:19 -0600

    Estructura para el logo del navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'height': 'height',
+        'width': 'width',
+        'spacing': 'margin, padding',
+      }
+    },
   },
   plugins: [],
 }
