71d532ed07a42589c4fa15be37756f8b4a4b9c10
Javi Maldonado <javimaldonadohdez@gmail.com>
2023-09-23 03:12:32 -0600

    Agregar funcion para agregar a los items del menú un indicador para saber que seccion se esta visualizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, OnInit, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
 import { NavbarComponent } from './components/navbar/navbar.component'
@@ -10,6 +10,58 @@ import { NavbarComponent } from './components/navbar/navbar.component'
   styleUrls: ['./app.component.css'],
   imports: [CommonModule, RouterOutlet, NavbarComponent],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Javi Maldonado'
+  private elementRef = inject(ElementRef)
+
+  ngOnInit(): void {
+    this.selectOptionMenu()
+  }
+
+  private createObserver(target: HTMLElement) {
+    const options = {
+      rootMargin: '-30% 0px -70% 0px',
+    }
+
+    const isIntersecting = (entry: IntersectionObserverEntry) =>
+      entry.isIntersecting || entry.intersectionRatio > 0
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const id = entry.target.getAttribute('id')
+        const menuLink = document.querySelector(`.menu a[href="#${id}"]`)
+
+        if (isIntersecting(entry)) {
+          const itemSelected = document.querySelector('.menu a.selected')
+          itemSelected?.classList.remove('selected')
+          menuLink?.classList.add('selected')
+        }
+      })
+    }, options)
+
+    observer.observe(target)
+  }
+
+  selectOptionMenu(): void {
+    try {
+      const menuItems =
+        this.elementRef.nativeElement.querySelectorAll('.menu a[href^="#"]')
+
+      menuItems.forEach((item: HTMLElement) => {
+        if (item) {
+          const hash = item.getAttribute('href')
+          console.log(`hash -  ${hash}`)
+          const target: HTMLElement =
+            this.elementRef.nativeElement.querySelector(`${hash}`)
+
+          if (target) {
+            this.createObserver(target)
+          }
+        }
+      })
+    } catch (error) {
+      console.error(' === JM === ')
+      console.error(error)
+    }
+  }
 }
